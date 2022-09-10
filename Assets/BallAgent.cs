9f3b2500592a1eab70d70661136233166af10bd6
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2022-09-09 19:28:14 -0700

    logic that moves agent back to start

--- Assets/BallAgent.cs
+++ Assets/BallAgent.cs
@@ -6,9 +6,24 @@ using Unity.MLAgents.Sensors;
 
 public class BallAgent : Agent
 {
+
+    public Transform Target;
+    Rigidbody rigidBody;
+
+    public override void OnEpisodeBegin()
+    {
+        if (this.transform.localPosition.y < 0)
+        {
+            this.rigidBody.angularVelocity = Vector3.zero;
+            this.rigidBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+        this.Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 }
