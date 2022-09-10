c15ddfe2d167158323a5d0cd644cd23213acf663
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2022-09-09 19:43:58 -0700

    actions processing logic added

--- Assets/BallAgent.cs
+++ Assets/BallAgent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
 public class BallAgent : Agent
 {
@@ -27,6 +28,16 @@ public class BallAgent : Agent
         sensor.AddObservation(this.rigidBody.velocity.x);
         sensor.AddObservation(this.rigidBody.velocity.z);
         sensor.AddObservation(this.Target.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        var actionX = actions.ContinuousActions[0];
+        var actionZ = actions.ContinuousActions[1];
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionX;
+        controlSignal.z = actionZ;
+        rigidBody.AddForce(controlSignal * 10);
     }
 
     // Start is called before the first frame update
