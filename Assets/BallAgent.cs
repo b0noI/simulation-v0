893552e0c0e9c1e2a1a2203e81472cd4be747397
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2022-09-09 19:15:40 -0700

    agent added

---
+++ Assets/BallAgent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class BallAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
